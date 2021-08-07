766cf63fb5e520193185534b9a80bbab40e73aa9
Branimir026 <br.belic@gmail.com>
2021-08-07 10:24:38 +0200

    BlogItems moved from App.js to it's own component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import Header from './components/Header';
-import BlogItem from './components/BlogItem';
+import "./App.css";
+import Header from "./components/Header";
+import BlogItems from "./components/BlogItems";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch(`http://localhost:8000/posts`)
-    .then(res => {
-      if(!res.ok) {
-        throw Error('Could not fetch the data.')
-      }
-      return res.json();
-    })
-    .then(data => {
-      setBlogs(data);
-      setIsLoading(false);
-      setError(null);
-    })
-    .catch(err => {
-      setError(err.message);
-      setIsLoading(false);
-    })
-  }, []);
-
   return (
     <div className="App">
       <Header />
-      {error && <h2>{ error }</h2>}
-      {isLoading && <h2>Loading...</h2>}
-      {blogs && blogs.map(blog => {
-        return (
-          <BlogItem blog={blog} key={blog.id} />
-        )
-      })}
+      <BlogItems />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/BlogItems.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import BlogItem from "./BlogItem";
+
+const BlogItems = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/posts`)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch the data.");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="blog_items_container">
+      {error && <h2>{error}</h2>}
+      {isLoading && <h2>Loading...</h2>}
+      {blogs &&
+        blogs.map((blog) => {
+          return <BlogItem blog={blog} key={blog.id} />;
+        })}
+    </div>
+  );
+};
+
+export default BlogItems;
