f35c9b7489b62820790cd7a28419490823b909cd
Branimir026 <br.belic@gmail.com>
2021-08-07 18:22:31 +0200

    Added Links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <Row className="header align-items-stretch">
-      <h1 className="align-self-center">Blog Post</h1>
+    <Row className="header align-items-center">
+      <Col>
+        <h1>
+          <Link to="/" className="logo">
+            Blog Post
+          </Link>
+        </h1>
+      </Col>
+      <Col>
+        <Link to="/addItem">
+          <Button variant="primary">Add post</Button>
+        </Link>
+      </Col>
     </Row>
   );
 };
