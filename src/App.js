d8b713cd047a4954d072c221c1e627d8519272a2
Branimir026 <br.belic@gmail.com>
2021-08-07 18:22:59 +0200

    Added Router, Switch and Routes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import "./App.css";
 
 import Container from "react-bootstrap/Container";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 import AddBlogItem from "./components/AddBlogItem";
 import BlogItems from "./components/BlogItems";
 
 const App = () => {
   return (
-    <Container>
-      <Header />
-      <AddBlogItem />
-      <BlogItems />
-    </Container>
+    <Router>
+      <Container>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={BlogItems} />
+          <Route path="/addItem" exact component={AddBlogItem} />
+        </Switch>
+      </Container>
+    </Router>
   );
 };
 
