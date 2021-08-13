67bc85331dd4d3689ea046dd01a8558254d2eb4b
Branimir026 <br.belic@gmail.com>
2021-08-13 12:11:51 +0200

    Added image to posts and displayed it

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -1,16 +1,28 @@
 import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 
 import { Link } from "react-router-dom";
 
 const BlogItem = ({ blog }) => {
   return (
     <Card className="text-center">
+      <Card.Img variant="top" src={blog.image} />
       <Card.Body>
-        <Card.Title>
-          <Link to={`/${blog.id}`}>{blog.title}</Link>
-        </Card.Title>
+        <Row>
+          <Col xs={{ span: 10, offset: 1 }} md={{ span: 6, offset: 0 }}>
+            <Card.Text className="text-muted">{blog.author}</Card.Text>
+          </Col>
+          <Col xs={{ span: 10, offset: 1 }} md={{ span: 6, offset: 0 }}>
+            <Card.Text className="text-muted">13.8.2021</Card.Text>
+          </Col>
+        </Row>
+        <Card.Title>{blog.title}</Card.Title>
         <Card.Text>{blog.text.slice(0, 250)}...</Card.Text>
-        <Card.Footer className="text-muted">{blog.author}</Card.Footer>
+        <Link to={`/${blog.id}`}>
+          <Button variant="primary">Read more</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
